10bbf33b26982913fe38ab82b578ad911ede2a03
frontenddeveloper-leejeongeun <49803637+frontend-leejeongeun@users.noreply.github.com>
2025-02-06 22:23:32 +0900

    Update .gitignore to exclude configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/frontend-leejeongeun.github.io",
+  assetPrefix: "/frontend-leejeongeun.github.io/",
 };
 
 export default nextConfig;
