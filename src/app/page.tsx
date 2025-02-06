77e23487405c2e776f3077dcb31b037f6e668ebd
frontenddeveloper-leejeongeun <49803637+frontend-leejeongeun@users.noreply.github.com>
2025-02-06 22:47:09 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>이정은 포트폴리오 페이지</li>
+          <li>이정은 룰루루</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
